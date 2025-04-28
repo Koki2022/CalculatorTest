3b90c776d3bf7ff4db91240cebcd9da5ab62d04c
Koki2022 <kokikoki3219@icloud.com>
2025-04-28 15:07:47 +0900

    プロジェクトアップロード

---
+++ CalculatorTest/CalculatorTest/CalculatorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTestApp.swift
+//  CalculatorTest
+//
+//  Created by 高橋昴希 on 2025/04/28.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorTest/CalculatorTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculatorTest
+//
+//  Created by 高橋昴希 on 2025/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
